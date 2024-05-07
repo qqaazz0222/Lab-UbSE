db88bfcf868a76cd2187882a82136e044aa6cced
qqaazz0222 <qqaazz0222@naver.com>
2024-05-07 12:18:17 +0900

    dashboard scroll top work #1

--- src/layouts/dashboard/layout.jsx
+++ src/layouts/dashboard/layout.jsx
@@ -2,16 +2,24 @@
 import { Outlet, useNavigate } from "react-router-dom";
 // 컴포넌트
 import Sidebar from "@/components/sidebar/component";
+// 아이콘
+import { ArrowUpFromLine } from "lucide-react";
 // 스타일
 import "./style.css";
 
 const DashboardLayout = () => {
+    const scrollTop = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    };
     return (
         <div id="dashboardLayout">
             <Sidebar />
             <div className="contentArea">
                 <Outlet />
             </div>
+            <button className="toTopBtn" onClick={scrollTop}>
+                <ArrowUpFromLine />
+            </button>
         </div>
     );
 };
